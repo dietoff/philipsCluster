f3f9403294098fbeba70a4c5939f0ba066abdcb1
dietmar offenhuber <dietoff@gmx.at>
2014-10-23 14:15:42 -0400

    facade bug solved, i hope

--- src/applets/Facade.java
+++ src/applets/Facade.java
@@ -1,69 +1,95 @@
 package applets;
 
+import java.awt.Graphics;
+
 import processing.core.PApplet;
+import processing.core.PGraphics;
 import processing.core.PImage;
 import eDMX.*;
 
 class Facade {
-  int numChainsTotal = 18;
-  int chainLength = 50;
-  PApplet p;
-  sACNSource source;
-  sACNUniverse[] universeArray = new sACNUniverse[numChainsTotal];
-  byte[][] chainDataArray = new byte[numChainsTotal][chainLength * 3];
+	int numChainsTotal = 18;
+	int chainLength = 50;
+	PApplet p;
+	sACNSource source;
+	sACNUniverse[] universeArray = new sACNUniverse[numChainsTotal];
+	byte[][] chainDataArray = new byte[numChainsTotal][chainLength * 3];
 
 
-  Facade(PApplet p_) {
-	 this.p = p_;
-    source = new sACNSource(p, "NEU Source");
-    // "Unit 1"
-    // NEU sPDS480-1
-    // IP Address: 10.1.3.230
-    int minUniverseUnit1 = 0;
-    int numChainsUnit1 = 8;
-    for (int i = minUniverseUnit1; i < minUniverseUnit1+numChainsUnit1; i++) {
-      universeArray[i] = new sACNUniverse(source, (short)(i+1));
-    }
-    // "Unit 2"
-    // NEU sPDS480-2
-    // IP Address: 10.1.3.231
-    int minUniverseUnit2 = 8;
-    int numChainsUnit2 = 8;
-    for (int i = minUniverseUnit2; i < minUniverseUnit2+numChainsUnit2; i++) {
-      universeArray[i] = new sACNUniverse(source, (short)(i+1));
-    }
-    // "Unit 3"
-    // NEU sPDS480-3
-    // IP Address: 10.1.3.232
-    int minUniverseUnit3 = 16;
-    int numChainsUnit3 = 2;
-    for (int i = minUniverseUnit3; i < minUniverseUnit3+numChainsUnit3; i++) {
-      universeArray[i] = new sACNUniverse(source, (short)(i+1));
-    }
-  }
+	Facade(PApplet p_) {
+		this.p = p_;
+		source = new sACNSource(p, "NEU Source");
+		// "Unit 1"
+		// NEU sPDS480-1
+		// IP Address: 10.1.3.230
+		int minUniverseUnit1 = 0;
+		int numChainsUnit1 = 8;
+		for (int i = minUniverseUnit1; i < minUniverseUnit1+numChainsUnit1; i++){
+			universeArray[i] = new sACNUniverse(source, (short)(i+1));
+		}
+		// "Unit 2"
+		// NEU sPDS480-2
+		// IP Address: 10.1.3.231
+		int minUniverseUnit2 = 8;
+		int numChainsUnit2 = 8;
+		for (int i = minUniverseUnit2; i < minUniverseUnit2+numChainsUnit2; i++){
+			universeArray[i] = new sACNUniverse(source, (short)(i+1));
+		}
+		// "Unit 3"
+		// NEU sPDS480-3
+		// IP Address: 10.1.3.232
+		int minUniverseUnit3 = 16;
+		int numChainsUnit3 = 2;
+		for (int i = minUniverseUnit3; i < minUniverseUnit3+numChainsUnit3; i++){
+			universeArray[i] = new sACNUniverse(source, (short)(i+1));
+		}
+	}
 
-  void toFacade(PImage chainPhoto) {
-	p.noSmooth();
-    p.image(chainPhoto, 0, 0, p.width, p.height);
-    for (int i = 0; i < numChainsTotal; i++) {
-      for (int j = 0; j < chainLength; j++) { 
-        chainDataArray[i][3*j + 0] = (byte)(p.red(chainPhoto.pixels[i*chainLength + j]));
-        chainDataArray[i][3*j + 1] = (byte)(p.green(chainPhoto.pixels[i*chainLength + j]));
-        chainDataArray[i][3*j + 2] = (byte)(p.blue(chainPhoto.pixels[i*chainLength + j]));
-      }
-    }
-    for (int i = 0; i < numChainsTotal; i++) {
-      universeArray[i].setSlots(0, chainDataArray[i]);
-    }
-    try {
-      for (int i = 0; i < numChainsTotal; i++) {
-        universeArray[i].sendData();
-      }
-    } 
-    catch (Exception e) {
-      e.printStackTrace();
-      p.exit();
-    }
-  }
+	void toFacade(PImage chainPhoto) {
+		for (int i = 0; i < numChainsTotal; i++) {
+			for (int j = 0; j < chainLength; j++) { 
+				int j2 = chainPhoto.pixels[i*chainLength + j];
+				chainDataArray[i][3*j + 0] = (byte)(p.red(j2));
+				chainDataArray[i][3*j + 1] = (byte)(p.green(j2));
+				chainDataArray[i][3*j + 2] = (byte)(p.blue(j2));
+			}
+		}
+		for (int i = 0; i < numChainsTotal; i++) {
+			universeArray[i].setSlots(0, chainDataArray[i]);
+		}
+		try {
+			for (int i = 0; i < numChainsTotal; i++) {
+				universeArray[i].sendData();
+			}
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+			p.exit();
+		}
+	}
+
+
+
+	void toFacade(PGraphics p) {
+		for (int i = 0; i < numChainsTotal; i++) {
+			for (int j = 0; j < chainLength; j++) {
+				int j2 = p.pixels[i*chainLength + j];
+				chainDataArray[i][3*j + 0] = (byte) p.red(j2);
+				chainDataArray[i][3*j + 1] = (byte) p.green(j2);
+				chainDataArray[i][3*j + 2] = (byte) p.blue(j2);
+			}
+		}
+		for (int i = 0; i < numChainsTotal; i++) {
+			universeArray[i].setSlots(0, chainDataArray[i]);
+		}
+		try {
+			for (int i = 0; i < numChainsTotal; i++) {
+				universeArray[i].sendData();
+			}
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
 
--- src/applets/RandomWalk2.java
+++ src/applets/RandomWalk2.java
@@ -1,9 +1,10 @@
 package applets;
 
 import processing.core.PApplet;
+import processing.core.PGraphics;
 import processing.core.PImage;
 
-public class RandomWalk extends PApplet {
+public class RandomWalk2 extends PApplet {
 	
 	Facade p = new Facade(this);
 	int w = 50;
@@ -11,30 +12,32 @@ public class RandomWalk extends PApplet {
 	float x = h/2;
 	float y = w/2;
 	float range = 4;
+	PGraphics pim;
 
 	public void setup() 
 	{
 	  size(w, h);
 	  background(0);
+	  pim = createGraphics(w,h);
 	}
 
 	public void draw() {
-	 fill(0,0,0,5); // fill screen with a transparent rectangle to dim previous strokes
-	 noStroke();
-	 rect(0,0,w,h);
+	 pim.fill(0,0,0,5); // fill screen with a transparent rectangle to dim previous strokes
+	 pim.noStroke();
+	 pim.rect(0,0,w,h);
 	 float newx = x+random(-range,range); // generate random destination
 	 float newy = y+random(-range,range);
 	 newx = constrain(newx,0,w); // make sure line stays on screen
 	 newy = constrain(newy,0,h);
 	 
-	 stroke(255);
-	 line(x,y,newx,newy); // draw new line segment
+	 pim.stroke(255);
+	 pim.line(x,y,newx,newy); // draw new line segment
 	 
 	 x= newx; // update starting point
 	 y= newy;
 	 
-	 PImage img = get();
-	 p.toFacade(img);
+	 
+	 p.toFacade(pim);
 	}
 }
 
